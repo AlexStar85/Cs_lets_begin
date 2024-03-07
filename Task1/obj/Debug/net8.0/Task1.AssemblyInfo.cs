96202121527e00a0894ba5095952ba61e49f34cb
Alex Star <N690@yandex.ru>
2024-03-08 01:21:38 +0300

    Добавили второй пример

--- Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1aeb64fbae34c9b6c18cdb8c5eb0f4e17b307ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67c0d719d41ac0769434c0fbc77dc3a4975f5fc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
