5d7c5e133e06b1698f3640e6283f34d6eac408f4
Alex Star <N690@yandex.ru>
2024-03-08 23:03:14 +0300

    Task5:Lesson

---
+++ Task5/Program.cs
@@ -0,0 +1,12 @@
+﻿// Дано натуральное число N. Вывести на экран числа от 1 N.
+
+Console.WriteLine("Введите значение целого числа N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i <= N)
+{
+    Console.Write(i);
+    Console.Write(' ');
+    i = i + 1;
+}
+ 
