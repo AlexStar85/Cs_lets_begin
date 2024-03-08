1d0212aae44b1c61f7a9f693d23b73d0b479aabc
Alex Star <N690@yandex.ru>
2024-03-08 22:05:31 +0300

    Решили Д/з из лекции

---
+++ TaskHomeWork/Program.cs
@@ -0,0 +1,13 @@
+﻿// Вычислить значение формулы (a*b)/(c+d)
+
+Console.WriteLine("Введите значение целого числа а:  ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение целого числа b:  ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение целого числа c:  ");
+int c = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение целого числа d:  ");
+double d = Convert.ToInt32(Console.ReadLine());
+
+double resalt = a*b/(c+d);
+Console.WriteLine(resalt);
