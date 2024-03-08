627c5672e44ad15bfb7ddf64cc564e7208d744df
Alex Star <N690@yandex.ru>
2024-03-08 22:30:21 +0300

    Задание 3 лелции

---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Даны два целых числа. Вывести на экран большее из них
+
+
+Console.WriteLine("Введите значение целого числа a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение целого числа b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+    Console.WriteLine(a);
+else
+    Console.WriteLine(b);
