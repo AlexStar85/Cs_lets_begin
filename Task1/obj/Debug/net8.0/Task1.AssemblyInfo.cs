6dcc0c654173e95e07d2081a6a8da64470bf6236
Alex Star <N690@yandex.ru>
2024-03-08 21:35:42 +0300

    Начинаем работу 8 марта

--- Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67c0d719d41ac0769434c0fbc77dc3a4975f5fc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96202121527e00a0894ba5095952ba61e49f34cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
